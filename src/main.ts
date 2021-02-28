1216de79421ff3984f92c05e485554e8b0d66915
Jim Strother <jim.strother@gmail.com>
2021-02-28 14:10:58 -0600

    Initial Commit

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import HelloWorldScene from './scenes/HelloWorldScene'
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
 	width: 800,
-	height: 600,
+	height: 640,
 	physics: {
 		default: 'arcade',
 		arcade: {
